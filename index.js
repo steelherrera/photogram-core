d0ebea083efaf731a5d52b0dc45cc5941448dce0
Steel Herrera <sherrera@formax.cl>
2019-04-11 11:57:07 -0500

    Added config exports

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
+const Config = require('./lib/config/configuration');
+module.exports.config = new Config();
 module.exports.DBConnector = require('./lib/connector/mysql-connector');
 module.exports.Utils = require('./lib/utils/util');

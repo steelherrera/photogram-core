b1ea395d5c2e0c6f6ef0f62c0edde1dc42c51657
Steel Herrera <sherrera@formax.cl>
2019-04-10 23:17:55 -0500

    Added utils

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ module.exports.config = new Config();
 module.exports.Logger = require('./lib/log/logger').Logger;
 module.exports.Transaction = require('./lib/log/transaction').Transaction;
 module.exports.TransactionAsync = require('./lib/log/transaction-async').Transaction;
-module.exports.Utils = require('./lib/utils/util');
 
 module.exports.CustomizableError = require('./lib/error/customizable-error');
 module.exports.isItPingQ = require('./lib/utils/ping');
@@ -13,4 +12,6 @@ module.exports.CalendarBuilderFactory = {
         return new CalendarBuilder();
     }
 };*/
+
 module.exports.DBConnector = require('./lib/connector/mysql-connector');
+module.exports.Utils = require('./lib/utils/util');

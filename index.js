d54df70142981be80f54474a9c00b5902505a93a
Steel Herrera <sherrera@formax.cl>
2019-04-11 10:17:59 -0500

    Added find

--- index.js
+++ index.js
@@ -1,17 +1,2 @@
-/*let Config = require('./lib/config/configuration');
-module.exports.config = new Config();
-module.exports.Logger = require('./lib/log/logger').Logger;
-module.exports.Transaction = require('./lib/log/transaction').Transaction;
-module.exports.TransactionAsync = require('./lib/log/transaction-async').Transaction;
-
-module.exports.CustomizableError = require('./lib/error/customizable-error');
-module.exports.isItPingQ = require('./lib/utils/ping');
-const CalendarBuilder = require('./lib/utils/calendar/CalendarBuilder').CalendarBuilder;
-module.exports.CalendarBuilderFactory = {
-    getBuilder: ()=>{
-        return new CalendarBuilder();
-    }
-};*/
-
 module.exports.DBConnector = require('./lib/connector/mysql-connector');
 module.exports.Utils = require('./lib/utils/util');
